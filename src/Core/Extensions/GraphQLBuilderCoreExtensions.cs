9f02bed187c60ef635da0877708cecf203a0149d
Shane Krueger <shane@acdmail.com>
2021-07-13 11:33:17 -0400

    Draft

--- src/Core/Extensions/GraphQLBuilderCoreExtensions.cs
+++ src/Core/Extensions/GraphQLBuilderCoreExtensions.cs
@@ -8,6 +8,7 @@ using GraphQL.Types.Relay;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
+using GraphQL.DI;
 
 namespace GraphQL.Server
 {
@@ -16,120 +17,6 @@ namespace GraphQL.Server
     /// </summary>
     public static class GraphQLBuilderCoreExtensions
     {
-        /// <summary>
-        /// Provides the ability to configure <see cref="ErrorInfoProviderOptions"/> for <see cref="DefaultErrorInfoProvider"/>.
-        /// </summary>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <param name="configureOptions">Action to configure the <see cref="ErrorInfoProviderOptions"/>.</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddErrorInfoProvider(this IGraphQLBuilder builder, Action<ErrorInfoProviderOptions> configureOptions)
-            => AddErrorInfoProvider<DefaultErrorInfoProvider>(builder, configureOptions);
-
-        /// <summary>
-        /// Provides the ability to configure <see cref="ErrorInfoProviderOptions"/> for the specified <typeparamref name="TErrorInfoProvider"/>.
-        /// </summary>
-        /// <typeparam name="TErrorInfoProvider">The <see cref="IErrorInfoProvider"/> implementation.</typeparam>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <param name="configureOptions">Action to configure the <see cref="ErrorInfoProviderOptions"/>.</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddErrorInfoProvider<TErrorInfoProvider>(this IGraphQLBuilder builder, Action<ErrorInfoProviderOptions> configureOptions)
-            where TErrorInfoProvider : DefaultErrorInfoProvider
-        {
-            if (configureOptions == null)
-                throw new ArgumentNullException(nameof(configureOptions));
-
-            return builder.AddErrorInfoProvider<TErrorInfoProvider>((opt, _) => configureOptions(opt));
-        }
-
-        /// <summary>
-        /// Provides the ability to configure <see cref="ErrorInfoProviderOptions"/> for <see cref="DefaultErrorInfoProvider"/>.
-        /// </summary>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <param name="configureOptions">Action to configure the <see cref="ErrorInfoProviderOptions"/>.</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddErrorInfoProvider(this IGraphQLBuilder builder, Action<ErrorInfoProviderOptions, IServiceProvider> configureOptions)
-            => AddErrorInfoProvider<DefaultErrorInfoProvider>(builder, configureOptions);
-
-        /// <summary>
-        /// Provides the ability to configure <see cref="ErrorInfoProviderOptions"/> for the specified <typeparamref name="TErrorInfoProvider"/>.
-        /// </summary>
-        /// <typeparam name="TErrorInfoProvider">The <see cref="IErrorInfoProvider"/> implementation.</typeparam>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <param name="configureOptions">Action to configure the <see cref="ErrorInfoProviderOptions"/>.</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddErrorInfoProvider<TErrorInfoProvider>(this IGraphQLBuilder builder, Action<ErrorInfoProviderOptions, IServiceProvider> configureOptions)
-            where TErrorInfoProvider : DefaultErrorInfoProvider
-        {
-            if (configureOptions == null)
-                throw new ArgumentNullException(nameof(configureOptions));
-
-            // This is used instead of "normal" services.Configure(configureOptions) to pass IServiceProvider to user code.
-            builder.Services.AddSingleton<IConfigureOptions<ErrorInfoProviderOptions>>(x => new ConfigureNamedOptions<ErrorInfoProviderOptions>(Options.DefaultName, opt => configureOptions(opt, x)));
-            builder.Services.TryAddSingleton<IErrorInfoProvider, TErrorInfoProvider>();
-            builder.Services.AddOptions();
-
-            return builder;
-        }
-
-        /// <summary>
-        /// Add required services for GraphQL data loader support
-        /// </summary>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddDataLoader(this IGraphQLBuilder builder)
-        {
-            builder.Services.TryAddSingleton<IDataLoaderContextAccessor, DataLoaderContextAccessor>();
-            builder.Services.AddSingleton<IDocumentExecutionListener, DataLoaderDocumentListener>();
-
-            return builder;
-        }
-
-        /// <summary>
-        /// Add all types that implement <seealso cref="IGraphType"/> in the calling assembly
-        /// </summary>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <param name="serviceLifetime">The service lifetime to register the GraphQL types with</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddGraphTypes(
-            this IGraphQLBuilder builder,
-            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
-            => builder.AddGraphTypes(Assembly.GetCallingAssembly(), serviceLifetime);
-
-        /// <summary>
-        /// Add all types that implement <seealso cref="IGraphType"/> in the assembly which <paramref name="typeFromAssembly"/> belongs to
-        /// </summary>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <param name="typeFromAssembly">The type from assembly to register all GraphQL types from</param>
-        /// <param name="serviceLifetime">The service lifetime to register the GraphQL types with</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddGraphTypes(
-            this IGraphQLBuilder builder,
-            Type typeFromAssembly,
-            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
-            => builder.AddGraphTypes(typeFromAssembly.Assembly, serviceLifetime);
-
-        /// <summary>
-        /// Add all types that implement <seealso cref="IGraphType"/> in the specified assembly
-        /// </summary>
-        /// <param name="builder">GraphQL builder used for GraphQL specific extension methods as 'this' argument.</param>
-        /// <param name="assembly">The assembly to register all GraphQL types from</param>
-        /// <param name="serviceLifetime">The service lifetime to register the GraphQL types with</param>
-        /// <returns>Reference to <paramref name="builder"/>.</returns>
-        public static IGraphQLBuilder AddGraphTypes(
-            this IGraphQLBuilder builder,
-            Assembly assembly,
-            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
-        {
-            // Register all GraphQL types
-            foreach (var type in assembly.GetTypes()
-                .Where(x => !x.IsAbstract && typeof(IGraphType).IsAssignableFrom(x)))
-            {
-                builder.Services.TryAdd(new ServiceDescriptor(type, type, serviceLifetime));
-            }
-
-            return builder;
-        }
-
         /// <summary>
         /// Adds the GraphQL Relay types <see cref="ConnectionType{TNodeType}"/>, <see cref="EdgeType{TNodeType}"/>
         /// and <see cref="PageInfoType"/>.
